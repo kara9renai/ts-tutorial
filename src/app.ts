a5e4bf4489186f17a64d657fa54dfc2b7aea818c
kara9renai <80733629+kara9renai@users.noreply.github.com>
2022-01-09 21:15:17 +0900

    ホットリロードの確認

--- src/app.ts
+++ src/app.ts
@@ -1,4 +1,4 @@
 import {Item} from './item';
 const elem = document.getElementById('output');
-const aBook = new Item('Introduction to TypeScript', 1980);
+const aBook = new Item('Introduction to TypeScript', 2080);
 aBook.say(elem);
